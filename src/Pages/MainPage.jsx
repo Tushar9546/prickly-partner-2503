1b24d71333f805196d493ec579588ecd4201bd0b
RohitMBelure <rohitbelure128@gmail.com>
2022-09-30 12:23:35 +0530

    updated data

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -6,34 +6,26 @@ import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import { ProductDetailPage } from "./ProductDetailPage";
 import { Women } from "./Women";
-
 import Account from "./Account";
 import CartPage from "./CartPage";
 import OrderSummaryPage from "./OrderSummaryPage";
 import PaymentPage from "./PaymentPage";
 import OrderConfirmedPage from "./OrderConfirmedPage";
 
-import Signup from "./Signup";
-
-
 export const MainPage = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/groceriesproducts" element={<GroceriesProducts />}/>
+        <Route path="/groceriesproducts" element={<GroceriesProducts />} />
         <Route path="/fashion/women" element={<Women />} />
         <Route path="/signin" element={<SignIn />} />
-
         <Route path="/signup" element={<SignUp />} />
         <Route path="/account" element={<Account />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/order" element={<OrderSummaryPage/>}/>
-        <Route path="/payment" element={<PaymentPage/>}/>
-        <Route path="/orderplaced" element={<OrderConfirmedPage/>}/>
-
-        <Route path="/signup" element={<Signup />} />
-
+        <Route path="/order" element={<OrderSummaryPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/orderplaced" element={<OrderConfirmedPage />} />
         <Route path="/fashion/women/:id" element={<ProductDetailPage />} />
       </Routes>
     </div>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -3,7 +3,6 @@ import {
   Button,
   Checkbox,
   Flex,
-  
   Image,
   Input,
   InputGroup,
@@ -18,27 +17,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { getLocalData } from "../Utils/localStorage";
 import { useNavigate } from "react-router-dom";
 
-
 const SignUp = () => {
   const dispatch = useDispatch();
   const users = useSelector((store) => store.authreducer.token);
   const [userinput, setuserinput] = useState({});
   const [savedata, setsavedata] = useState({});
   let phono = getLocalData("Phone");
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
   const handleuserchange = (e) => {
     const { name, value } = e.target;
     setuserinput({ ...userinput, phnop: phono, [name]: value });
-    // console.log(userinput)
   };
 
   const handleformsubmit = (e) => {
     e.preventDefault();
     if (userinput.password === userinput.repassword) {
-      dispatch(postauth(userinput))
-      alert("signup successfull")
-      navigate("/signin")
+      dispatch(postauth(userinput));
+      alert("signup successfull");
+      navigate("/signin");
     } else {
       alert("password dosn't match");
     }
@@ -48,7 +45,6 @@ const SignUp = () => {
     dispatch(getauth());
   }, []);
 
-  //  console.log(users)
   const [show, setShow] = React.useState(false);
   const handleClick = () => setShow(!show);
   const [show2, setShow2] = React.useState(false);
@@ -56,41 +52,49 @@ const SignUp = () => {
 
   return (
     <Box>
-    <Box >
-   
-  </Box>
-    <Box m="200px">
-      <Flex  gap="150px">
-        <Box >
-          <Image borderRadius="10px 0px 0px 0px"
-            src="https://www.jiomart.com/msassets/images/login-banner.jpg"
-            alt="image"
-          />
-        </Box>
-        <form onSubmit={handleformsubmit}>
-          <Text fontSize="2xl" fontWeight="semibold" color="black" pb="6px" mt="20px">Sign up</Text>
-          <Text pb="40px" fontSize="sm" color="#978871" fontWeight="medium">Please enter your details.</Text>
-          <Stack mb="35px" spacing={10}>
-            <Input
-              variant="outline"
-              placeholder="Your First Name"
-              name="fistname"
-              onChange={handleuserchange}
+      <Box></Box>
+      <Box m="200px">
+        <Flex gap="150px">
+          <Box>
+            <Image
+              borderRadius="10px 0px 0px 0px"
+              src="https://www.jiomart.com/msassets/images/login-banner.jpg"
+              alt="image"
             />
-            <Input
-              variant="outline"
-              placeholder="Your Last Name"
-              name="lastname"
-              onChange={handleuserchange}
-            />
-            <Input
-            
-              variant="outline"
-              placeholder="Enter your Email Id"
-              name="email"
-              onChange={handleuserchange}
-            />
-               </Stack>
+          </Box>
+          <form onSubmit={handleformsubmit}>
+            <Text
+              fontSize="2xl"
+              fontWeight="semibold"
+              color="black"
+              pb="6px"
+              mt="20px"
+            >
+              Sign up
+            </Text>
+            <Text pb="40px" fontSize="sm" color="#978871" fontWeight="medium">
+              Please enter your details.
+            </Text>
+            <Stack mb="35px" spacing={10}>
+              <Input
+                variant="outline"
+                placeholder="Your First Name"
+                name="fistname"
+                onChange={handleuserchange}
+              />
+              <Input
+                variant="outline"
+                placeholder="Your Last Name"
+                name="lastname"
+                onChange={handleuserchange}
+              />
+              <Input
+                variant="outline"
+                placeholder="Enter your Email Id"
+                name="email"
+                onChange={handleuserchange}
+              />
+            </Stack>
             <InputGroup size="md">
               <Input
                 pr="4.5rem"
@@ -103,17 +107,13 @@ const SignUp = () => {
                 <Button h="1.75rem" size="sm" onClick={handleClick}>
                   {show ? <ViewOffIcon /> : <ViewIcon />}
                 </Button>
-              
               </InputRightElement>
-             
             </InputGroup>
-            <Text fontSize="13px" color="##e3c6b0">Use 8 or more characters with a mix of letters & numbers</Text>
-       
-         
-           
-           
+            <Text fontSize="13px" color="##e3c6b0">
+              Use 8 or more characters with a mix of letters & numbers
+            </Text>
 
-            <InputGroup  mt="30px"  size="md">
+            <InputGroup mt="30px" size="md">
               <Input
                 pr="4.5rem"
                 name="repassword"
@@ -127,34 +127,51 @@ const SignUp = () => {
                 </Button>
               </InputRightElement>
             </InputGroup>
-          
-          <Checkbox mt="35px" defaultChecked>Enable order updates and important information on Whatsapp</Checkbox>
-          <Text fontSize="2xl" fontWeight="semibold" color="black" mt="35px">Verify</Text>
-          <Text fontSize="13px"  color="#
-#e3c6b0">We have sent 6 digit OTP on <span style={{color:"black",fontWeight:"bolder"}}>{phono} </span></Text>
-          <Input
-          mt="35px"
-            variant="outline"
-            placeholder="Enter your OTP"
-            name="otp"
-            onChange={handleuserchange}
-          />
-          <Button w="100%" mt="30px"   _hover={{
-    background: "008ecc",
-    color: "white",
-  }} bg="#008ecc" color="white" type="submit">Verify</Button>
-        </form>
-      </Flex>
-      <Box textAlign="center" pt="50px">
-        <Text fontSize="small" fontWeight="normal">
-          By continuing you agree to our <span style={{color:"red"}}>Terms of service</span>
-        </Text>
-        <Text fontSize="small" >
-          and <span style={{color:"red"}}>Privacy & Legal Policy.</span>
-        </Text>
+
+            <Checkbox mt="35px" defaultChecked>
+              Enable order updates and important information on Whatsapp
+            </Checkbox>
+            <Text fontSize="2xl" fontWeight="semibold" color="black" mt="35px">
+              Verify
+            </Text>
+            <Text fontSize="13px" color="#e3c6b0">
+              We have sent 6 digit OTP on{" "}
+              <span style={{ color: "black", fontWeight: "bolder" }}>
+                {phono}{" "}
+              </span>
+            </Text>
+            <Input
+              mt="35px"
+              variant="outline"
+              placeholder="Enter your OTP"
+              name="otp"
+              onChange={handleuserchange}
+            />
+            <Button
+              w="100%"
+              mt="30px"
+              _hover={{
+                background: "008ecc",
+                color: "white",
+              }}
+              bg="#008ecc"
+              color="white"
+              type="submit"
+            >
+              Verify
+            </Button>
+          </form>
+        </Flex>
+        <Box textAlign="center" pt="50px">
+          <Text fontSize="small" fontWeight="normal">
+            By continuing you agree to our{" "}
+            <span style={{ color: "red" }}>Terms of service</span>
+          </Text>
+          <Text fontSize="small">
+            and <span style={{ color: "red" }}>Privacy & Legal Policy.</span>
+          </Text>
+        </Box>
       </Box>
-    </Box>
-    
     </Box>
   );
 };
